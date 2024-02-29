3b744d937b1381bf65f8bdfbd6aef1acadc2ed45
mulamuleli-simba <65784695+mulamuleli-simba@users.noreply.github.com>
2024-02-29 15:34:00 +0200

    Remove service_account
    remove service_account for security reasons

--- deploying infra with terr/variables.tf
+++ deploying infra with terr/variables.tf
@@ -1,3 +1,8 @@
 /*
 variable "project_name" {}
+"project_id": "var.project_id",
+"private_key_id": "var.private_key_id",
+"private_key": "var.private_key",
+"client_email": "var.client_email",
+"client_id": "var.client_id",
 */
